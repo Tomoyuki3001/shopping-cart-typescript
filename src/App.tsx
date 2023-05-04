b0536e5e7b0992cac9def8cb5748317dea84ed50
Tomoyuki Fujii <mjr01300828@gmail.com>
2023-05-04 15:29:37 -0700

    This is first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom"
+import {Container} from 'react-bootstrap'
+
+
+function App() {
+  return (
+    <>
+      <Container>Hi</Container>
+    </>
+  )
+}
+
+export default App

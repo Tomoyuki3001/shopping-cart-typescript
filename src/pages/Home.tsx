8f261563ab91a95f88758fdea7107ed2972fc0b7
Tomoyuki Fujii <mjr01300828@gmail.com>
2023-05-09 12:38:46 -0700

    Added some sentences

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,20 @@
+import { Button, Nav } from "react-bootstrap"
+import { NavLink } from 'react-router-dom'
+
 export default function Home() {
-    return <h1>Home</h1>
+    return (
+    <div>
+        <h1>Home</h1>
+        <p style={{fontSize: "1.5rem"}}>This website is a shopping-cart site clone with TypeScript and Json data. A user can save the shopping data each time.</p>
+        <div className=" d-flex justify-content-center">
+            <img style={{width: '50%', height: 'auto'}} src="./public/imgs/store.png" alt="" />
+        </div>
+        <div className=" d-flex justify-content-center">
+        <p style={{fontSize: "2rem", padding:'5% 0% 0% 0%'}}>Just play around click the "Store" button below.</p>
+        </div>
+        <div className=" d-flex justify-content-center">
+        <Button><Nav.Link to='/store' as={NavLink} >Store</Nav.Link></Button>
+        </div>
+    </div>
+    )
 }
